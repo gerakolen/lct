7808d7637025256104696575c0814913d72a8f7a
Eugene Tolbakov <ev.tolbakov@gmail.com>
2025-09-29 09:29:49 +0100

    feat: read/write TaskResultResponse into task table (#12)

--- tests/app/routers/test_task.py
+++ tests/app/routers/test_task.py
@@ -136,7 +136,28 @@ def test_get_result_unsuccessful(client, router_session_mock, monkeypatch):
 def test_get_result_successful(client, router_session_mock, monkeypatch):
     mock_task = MagicMock(name="Task")
     mock_task.get.return_value = Task(
-        id=TEST_TASK_ID, status=TaskStatus.COMPLETE, result={"info": "success"}
+        id=TEST_TASK_ID,
+        status=TaskStatus.COMPLETE,
+        result={
+            "ddl": [
+                {
+                    "statement": "CREATE TABLE quests.public.h_author ( id integer, name varchar, created_at timestamp(6));"
+                }
+            ],
+            "migrations": [
+                {
+                    "statement": "INSERT INTO quests.public.h_author_new SELECT * FROM quests.public.h_author;"
+                }
+            ],
+            "queries": [
+                {
+                    "queryid": "3b1cc90f-d446-4592-becd-8c26efbabf56",
+                    "query": "WITH raw_data AS (SELECT ha.id AS author_id, ha.name AS author_name, ep.excursion_id, qp.quest_id, ei.amount AS exc_amount, qi.amount AS quest_amount FROM quests.public.h_author ha LEFT JOIN quests.public.l_excursion_author ea ON ha.id = ea.author_id LEFT JOIN quests.public.l_excursion_payment ep ON ea.excursion_id = ep.excursion_id LEFT JOIN quests.public.s_payment_info ei ON ep.payment_id = ei.payment_id LEFT JOIN quests.public.l_author_quest aq ON ha.id = aq.author_id LEFT JOIN quests.public.l_quest_payment qp ON aq.quest_id = qp.quest_id LEFT JOIN quests.public.s_payment_info qi ON qp.payment_id = qi.payment_id LIMIT 500000), dup AS (SELECT r.*, x.n AS mult FROM raw_data r CROSS JOIN (VALUES 1,2) AS x(n)), calc AS (SELECT author_id, author_name, COUNT(excursion_id) + COUNT(quest_id) AS total_sales, COALESCE(SUM(exc_amount),0) + COALESCE(SUM(quest_amount),0) AS total_revenue, ROW_NUMBER() OVER (PARTITION BY author_id ORDER BY random()) AS rn, AVG(exc_amount) OVER (PARTITION BY author_id) AS avg_exc, MAX(quest_amount) OVER (PARTITION BY author_id) AS max_quest FROM dup GROUP BY author_id, author_name, excursion_id, quest_id, exc_amount, quest_amount) SELECT author_id, author_name, total_sales, total_revenue, avg_exc, max_quest FROM calc WHERE rn < 500 ORDER BY random();",
+                    "runquantity": 44,
+                    "executiontime": 25,
+                }
+            ],
+        },
     )
     app.dependency_overrides[get_session] = lambda: mock_task
     app.dependency_overrides[require_basic_auth] = lambda: "test-user"
