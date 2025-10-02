0d48eb4ffb42a7e63b9d508bfe5a599ccc77e74b
Eugene Tolbakov <ev.tolbakov@gmail.com>
2025-10-02 20:59:13 +0100

    chore: remove unused (#18)

--- app/worker_task.py
+++ app/worker_task.py
@@ -8,7 +8,6 @@ from celery.signals import worker_process_init, worker_process_shutdown
 
 from sqlalchemy.orm import sessionmaker, Session
 
-from app.analyze.analyze_sql import analyze_sql
 from app.analyze.sql_static import build_context_pack
 from app.client.trino_client import extract_connection_details
 from app.client.yandex_client import call_yandex
@@ -76,38 +75,9 @@ def _session() -> Session:
     return _SessionLocal()
 
 
-def mock_response_for_do_work() -> Dict[str, Any]:
-    return {
-        "ddl": [
-            {
-                "statement": "CREATE TABLE quests.public.h_author ( id integer, name varchar, created_at timestamp(6));"
-            }
-        ],
-        "migrations": [
-            {
-                "statement": "INSERT INTO quests.public.h_author_new SELECT * FROM quests.public.h_author;"
-            }
-        ],
-        "queries": [
-            {
-                "queryid": "3b1cc90f-d446-4592-becd-8c26efbabf56",
-                "query": "WITH raw_data AS (SELECT ha.id AS author_id, ha.name AS author_name, ep.excursion_id, qp.quest_id, ei.amount AS exc_amount, qi.amount AS quest_amount FROM quests.public.h_author ha LEFT JOIN quests.public.l_excursion_author ea ON ha.id = ea.author_id LEFT JOIN quests.public.l_excursion_payment ep ON ea.excursion_id = ep.excursion_id LEFT JOIN quests.public.s_payment_info ei ON ep.payment_id = ei.payment_id LEFT JOIN quests.public.l_author_quest aq ON ha.id = aq.author_id LEFT JOIN quests.public.l_quest_payment qp ON aq.quest_id = qp.quest_id LEFT JOIN quests.public.s_payment_info qi ON qp.payment_id = qi.payment_id LIMIT 500000), dup AS (SELECT r.*, x.n AS mult FROM raw_data r CROSS JOIN (VALUES 1,2) AS x(n)), calc AS (SELECT author_id, author_name, COUNT(excursion_id) + COUNT(quest_id) AS total_sales, COALESCE(SUM(exc_amount),0) + COALESCE(SUM(quest_amount),0) AS total_revenue, ROW_NUMBER() OVER (PARTITION BY author_id ORDER BY random()) AS rn, AVG(exc_amount) OVER (PARTITION BY author_id) AS avg_exc, MAX(quest_amount) OVER (PARTITION BY author_id) AS max_quest FROM dup GROUP BY author_id, author_name, excursion_id, quest_id, exc_amount, quest_amount) SELECT author_id, author_name, total_sales, total_revenue, avg_exc, max_quest FROM calc WHERE rn < 500 ORDER BY random();",
-                "runquantity": 44,
-                "executiontime": 25,
-            }
-        ],
-    }
-
-
-# --- Business logic  ---
 def _do_work(payload: Dict[str, Any]) -> Dict[str, Any]:
     queries = payload.get("queries", "")
     ddl = payload.get("ddl", [])
-
-    # logger.info(f"Queries: {queries}, DDL: {ddl}")
-    # jdbc_url = payload.get("url", "")
-    # trino_settings = extract_connection_details(jdbc_url)
-    # logger.info(f"Trino settings: {trino_settings}")
 
     context_pack = build_context_pack(ddl=ddl, queries=queries)
     requirements = {
@@ -117,7 +87,6 @@ def _do_work(payload: Dict[str, Any]) -> Dict[str, Any]:
     }
     result = call_yandex(context_pack, payload, requirements=requirements)
     return result
-
 
 @celery_app.task(
     bind=True,
